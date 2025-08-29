b52c7f5e9abe5abc31b8212ee6e6687d1f4642b3
zhang Julie <jl.zhang@vdx.com>
2025-08-29 17:01:32 +0800

    feature: cici

---
+++ packages/sdk/src/common/transfer.ts
@@ -0,0 +1,20 @@
+import { ChainId } from "@/config/chain";
+import { getERC20Contract } from "@/utils/web3/providers";
+
+export const transfer = async (chainId: ChainId, tokenAddress: string,approveAddress: string, amount:bigint) => {
+  try {
+    const TokenContract = await getERC20Contract(chainId, tokenAddress);
+    // Avoid the user's multiple authorization amount, and the maximum amount will be authorized by default
+    const response = await TokenContract.transfer(approveAddress, amount)
+    console.log("transfer amount", amount)
+    
+    // Wait for block confirmation
+    const receipt = await response?.wait()
+    
+    console.log(receipt)
+    
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
