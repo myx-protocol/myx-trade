f788a0ffcb863bc37522e65c3f7fa65a4cc52c00
ryan <fanjiachangryan@163.com>
2025-09-01 15:58:18 +0800

    init

---
+++ src/trade/type.ts
@@ -0,0 +1,36 @@
+import { ChainId } from "@/config/chain";
+import { AddressLike, Signer } from "ethers";
+import { OrderType, TriggerType, OperationType, Direction, TimeInForce } from "@/config/con";
+
+
+
+export type UserFeeRateParams = {
+  address: AddressLike;
+  poolId: string;
+  chainId: ChainId;
+  singer: Signer;
+}
+
+export type PlaceOrderParams = {
+  chainId: ChainId;
+  address: AddressLike;
+  poolId: string;
+  positionId: number;
+  orderType: OrderType;
+  triggerType: TriggerType;
+  operation: OperationType;
+  direction: Direction;
+  collateralAmount: string;
+  size: string;
+  orderPrice: string;
+  triggerPrice: string;
+  timeInForce: TimeInForce;
+  postOnly: boolean;
+  slippagePct: string;
+  executionFeeToken:  AddressLike;
+  leverage: number;
+  tpSize: string;
+  tpPrice: string;
+  slSize: string;
+  slPrice: string;
+}
