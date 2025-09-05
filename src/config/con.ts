e1ad14124552e317c1d6e2ae9ee598ca2eda3f2a
ryan <fanjiachangryan@163.com>
2025-09-05 11:06:21 +0800

    feat: update

--- src/config/con.ts
+++ src/config/con.ts
@@ -1,29 +1,38 @@
-export enum OrderType {
-  MARKET = 0,
-  LIMIT = 1,
-  STOP = 2, // TPSL
-  CONDITIONAL // CPSL
-}
+export const OrderType = {
+  MARKET: 0,
+  LIMIT: 1,
+  STOP: 2, // TPSL
+  CONDITIONAL: 3 // CPSL
+} as const;
 
-export enum TriggerType {
-  NONE,
-  GTE,
-  LTE
-}
+export type OrderType = typeof OrderType[keyof typeof OrderType];
 
-export enum OperationType {
-  INCREASE = 0,
-  DECREASE = 1,
-}
+export const TriggerType = {
+  NONE: 0,
+  GTE: 1,
+  LTE: 2
+} as const;
 
-export enum Direction {
-  LONG,
-  SHORT
-}
+export type TriggerType = typeof TriggerType[keyof typeof TriggerType];
 
+export const OperationType = {
+  INCREASE: 0,
+  DECREASE: 1,
+} as const;
 
-export enum TimeInForce {
-  IOC = 0,
-}
+export type OperationType = typeof OperationType[keyof typeof OperationType];
 
-export const TIME_IN_FORCE: TimeInForce = TimeInForce.IOC
+export const Direction = {
+  LONG: 0,
+  SHORT: 1
+} as const;
+
+export type Direction = typeof Direction[keyof typeof Direction];
+
+export const TimeInForce = {
+  IOC: 0,
+} as const;
+
+export type TimeInForce = typeof TimeInForce[keyof typeof TimeInForce];
+
+export const TIME_IN_FORCE: TimeInForce = TimeInForce.IOC;
--- src/trade/index.ts
+++ src/trade/index.ts
@@ -12,7 +12,52 @@ export const getUserFeeRate = async ({ address, poolId, chainId }: UserFeeRatePa
 }
 
 export const placeOrder = async (params: PlaceOrderParams, singer: Signer) => {
+  // todo
+  // 计算订单金额
+  
+  // 计算执行费用
+  
+  // 划点 
+  
+  // 划点成功 检查钱包是否充足
+  
+  // 检查钱包充足 执行订单  
+  
+  // 执行订单成功 检查订单是否成功
+  
+  // gas
+  
+  console.log('params--->', JSON.stringify(params))
+  // 执行订单
   const brokerContract = await getBrokerSingerContract(params.chainId, singer);
+
+  // @ts-ignore
+  const gasLimit = await brokerContract.placeOrder.estimateGas(
+    {
+      user: params.address,
+      poolId: params.poolId,
+      positionId: params.positionId,
+      orderType: params.orderType,
+      triggerType: params.triggerType,
+      operation: params.operation,
+      direction: params.direction,
+      collateralAmount: params.collateralAmount,
+      size: params.size,
+      orderPrice: params.orderPrice,
+      triggerPrice: params.triggerPrice,
+      timeInForce: TIME_IN_FORCE,
+      postOnly: params.postOnly,
+      slippagePct: params.slippagePct,
+      executionFeeToken: params.executionFeeToken,
+      leverage: params.leverage,
+      tpSize: params.tpSize,
+      tpPrice: params.tpPrice,
+      slSize: params.slSize,
+      slPrice: params.slPrice,
+    }
+  )
+  console.log('gasLimit--->', gasLimit)
+
   const response = await brokerContract.placeOrder(
     {
       user: params.address,
