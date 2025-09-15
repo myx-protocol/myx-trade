eb2037e4953c01c8b9442595006a641db3a4b8ca
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2025-09-15 23:02:02 +0800

    feature: deposit

--- packages/playground/src/components/DepositQuote.tsx
+++ packages/playground/src/components/DepositQuote.tsx
@@ -4,7 +4,7 @@ import { usePoolInfo } from "./PoolInfo";
 import { Button } from "@/components";
 import { Market , quote} from "@myx-trade/sdk";
 
-export const DepositQuote = () => {
+export const DepositQuote = ({className = ''}: {className?: string}) => {
   const {chainId} = useContext(PoolContext)
   const {poolId} = usePoolInfo()
   const [amount, setAmount] = useState<string>(Market[chainId].poolPrimeThreshold.toString())
@@ -14,11 +14,11 @@ export const DepositQuote = () => {
     if (!poolId || !amount || !slippage) return
     await quote.deposit({chainId, poolId, amount: Number(amount), slippage: Number(slippage) })
   },[])
-  return <div className={'flex flex-col gap-[10px]'}>
+  return <div className={`flex flex-col gap-[10px] ${className}`}>
     {/*<div>poolId: {poolId}</div>*/}
     <div className={'flex gap-[10px] items-center'}>
       <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input type="number" className={'border-1'} readOnly={true} value={slippage} /></div>
-      <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input type="number" className={'border-1'}  onChange={e => setAmount(e.target.value)} value={amount} placeholder={'Amount'} /></div>
+      <div className={'flex items-center gap-[5px]'}><label>Amount: </label><input type="number" className={'border-1'}  onChange={e => setAmount(e.target.value)} value={amount} placeholder={'Amount'} /></div>
       <Button label={'DepositQuote'} onClick={onHandleDepositQuote}/>
     </div>
   </div>
