9af7a4dc87921abc491559cd3bbaa781141c4713
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2025-09-18 17:58:55 +0800

    feature: transfer

--- packages/playground/src/components/DepositBase.tsx
+++ packages/playground/src/components/DepositBase.tsx
@@ -23,8 +23,8 @@ export const DepositBase = () => {
   return <div className={'flex flex-col gap-[10px]'}>
     {/*<div>poolId: {poolId}</div>*/}
     <div className={'flex gap-[10px] items-center'}>
-      <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input type="number" className={'border-1'} readOnly={true} value={slippage} /></div>
-      <div className={'flex items-center gap-[5px]'}><label>Amount:</label><input type="number" className={'border-1'}  onChange={e => setAmount(e.target.value)} value={amount} placeholder={'Amount'} /></div>
+      <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input type="number" className={'border-1 p-[8px]'} readOnly={true} value={slippage} /></div>
+      <div className={'flex items-center gap-[5px]'}><label>Amount:</label><input type="number" className={'border-1 p-[8px]'}  onChange={e => setAmount(e.target.value)} value={amount} placeholder={'Amount'} /></div>
       <Button label={'DepositBase'} onClick={onHandleDeposit}/>
       <Button label={'WithdrawBase'} onClick={onHandleWithdraw}/>
     </div>
