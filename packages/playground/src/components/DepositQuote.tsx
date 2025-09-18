157b08536460cb2215b9ca5b78868399735ee17b
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2025-09-18 22:22:27 +0800

    feature: message loading

--- packages/playground/src/components/DepositQuote.tsx
+++ packages/playground/src/components/DepositQuote.tsx
@@ -3,25 +3,42 @@ import { PoolContext } from "./PoolContext";
 import { usePoolInfo } from "./PoolInfo";
 import { Button } from "@/components";
 import { Market , quote} from "@myx-trade/sdk";
+import { message } from "antd";
 
 export const DepositQuote = ({className = ''}: {className?: string}) => {
   const {chainId} = useContext(PoolContext)
   const {poolId} = usePoolInfo()
   const [amount, setAmount] = useState<string>(Market[chainId].poolPrimeThreshold.toString())
   const [slippage] = useState<string>('0.01')
+  const [isDepositLoading, setIsDepositLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   
   const onHandleDepositQuote = useCallback(async () => {
     console.log("depositQuote", poolId, amount, slippage)
     if (!poolId || !amount || !slippage) return
     console.log("depositQuote", poolId, amount)
-    await quote.deposit({chainId, poolId, amount: Number(amount), slippage: Number(slippage) })
+    try {
+      setIsDepositLoading(true)
+      await quote.deposit({chainId, poolId, amount: Number(amount), slippage: Number(slippage) })
+      message.success("Deposit success")
+    } finally {
+      setIsDepositLoading(false)
+    }
+   
   },[poolId, amount, slippage])
   
   const onHandleWithdraw = useCallback(async () => {
     console.log("WithdrawQuote", poolId, amount, slippage)
     if (!poolId || !amount || !slippage) return
     console.log("WithdrawQuote", poolId, amount)
-    await quote.withdraw({chainId, poolId, amount: Number(amount), slippage: Number(slippage) })
+    try {
+      setIsLoading(true)
+      await quote.withdraw({chainId, poolId, amount: Number(amount), slippage: Number(slippage) })
+      message.success("Withdraw success")
+    } finally {
+      setIsLoading(false)
+    }
+    
   },[poolId, amount, slippage])
   
   return <div className={`flex flex-col gap-[10px] ${className}`}>
@@ -29,8 +46,8 @@ export const DepositQuote = ({className = ''}: {className?: string}) => {
     <div className={'flex gap-[10px] items-center'}>
       <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input type="number" className={'border-1 p-[8px]'} readOnly={true} value={slippage} /></div>
       <div className={'flex items-center gap-[5px]'}><label>Amount: </label><input type="number" className={'border-1 p-[8px]'}  onChange={e => setAmount(e.target.value)} value={amount} placeholder={'Amount'} /></div>
-      <Button label={'DepositQuote'} onClick={onHandleDepositQuote}/>
-      <Button label={'WithdrawQuote'} onClick={onHandleWithdraw}/>
+      <Button label={'DepositQuote'} isLoading={isDepositLoading} onClick={onHandleDepositQuote}/>
+      <Button label={'WithdrawQuote'} isLoading={isLoading} onClick={onHandleWithdraw}/>
     </div>
   </div>
 }
