d3db291456b414b3e280f286a81a962b58d49511
allen <allen@myx.finance>
2025-09-25 20:55:37 +0800

    feat: 🎸 add docs app

---
+++ apps/docs/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/allen/work/mx-trade-sdk/apps/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/allen/work/mx-trade-sdk/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/allen/work/mx-trade-sdk/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.11_bufferutil@4.0.9_react-dom@19.1.1__22f6e9bd23f719007857bc8f255e16cf/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/allen/work/mx-trade-sdk/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.11_bufferutil@4.0.9_react-dom@19.1.1__22f6e9bd23f719007857bc8f255e16cf/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/allen/work/mx-trade-sdk/apps/docs/src/css/custom.css"),
+];
