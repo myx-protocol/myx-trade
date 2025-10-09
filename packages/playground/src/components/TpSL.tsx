93e660acbd1ba939a7db08298a0a4ab505fe77da
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2025-10-09 17:04:33 +0800

    feature: type

--- packages/playground/src/components/TpSL.tsx
+++ packages/playground/src/components/TpSL.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useContext, useEffect, useState } from "react";
 import { PoolContext, TpSlContext } from "./PoolContext";
 import { usePoolInfo } from "./PoolInfo";
-import { pool as Pool } from "@myx-trade/sdk";
-import { message } from "antd";
+import {pool as Pool } from "@myx-trade/sdk";
+import { message, Radio, type RadioChangeEvent } from "antd";
 import { Button } from "@components/Button.tsx";
+// import { formatUnits } from "ethers/lib.esm";
 
 export const TpOrSL = ({className = '', amount = '', triggerPrice = '', onAmountChange , onPriceChange } : {className?: string, amount?: string | number, triggerPrice?: string|number,onAmountChange?: (value:string | number) => void ,onPriceChange?: (value:string | number) => void }) => {
   return <div className={`flex flex-col gap-[10px] ${className}`}>
@@ -24,6 +25,7 @@ export const TpSL = ({className = ''}: {className?: string}) => {
   const [slAmount, setSlAmount] = useState<string | number>("")
   const [slPrice, setSlPrice] = useState<string | number>("")
   const [slippage] = useState<string>('0.01')
+  const [poolType, setPoolType] = useState<Pool.PoolType>(Pool.PoolType.Base)
   
   
   
@@ -46,7 +48,7 @@ export const TpSL = ({className = ''}: {className?: string}) => {
       slippage: Number(slippage),
       poolId,
       chainId,
-      poolType: Pool.PoolType.Base,
+      poolType: poolType,
       tpsl
     }
     console.log("addTpsl params:", params)
@@ -61,8 +63,32 @@ export const TpSL = ({className = ''}: {className?: string}) => {
       setLoading(false)
     }
     
-  },[poolId, tpPrice,tpAmount, slAmount,slPrice, slippage])
+  },[poolId, tpPrice,tpAmount, slAmount,slPrice, slippage, poolType])
   
+  const onChange = (e: RadioChangeEvent) => {
+    setPoolType(e.target.value);
+  };
+  
+  /*const getBasePoolBalance = async () => {
+    setTpAmount('')
+    setSlAmount('')
+    if (!poolId || !account) return
+    if (pool?.quoteToken && account) {
+      const bigintBalance = await getBalanceOf(chainId, account, pool?.basePoolToken)
+      const _balance = formatUnits(bigintBalance, Market[chainId].lpDecimals)
+      setBasePoolBalance(_balance)
+    }
+  }
+  
+  const getQuotePoolBalance = async () => {
+    setQuotePoolBalance('')
+    if (!poolId || !account) return
+    if (pool?.quoteToken && account) {
+      const bigintBalance = await getBalanceOf(chainId, account, pool?.quotePoolToken)
+      const _balance = formatUnits(bigintBalance,Market[chainId].lpDecimals)
+      setQuotePoolBalance(_balance)
+    }
+  }*/
   
   useEffect(() => {
     if (pool) {
@@ -77,6 +103,16 @@ export const TpSL = ({className = ''}: {className?: string}) => {
       
       <div className={'flex gap-[10px] flex-col'}>
         <div className={'flex items-center gap-[5px]'}><label>Slippage: </label><input  readOnly={true} className={'border-1 flex-1 p-[8px] '}  value={slippage} /></div>
+        <div className={'flex items-center gap-[5px]'}><label>Type:</label>
+          <Radio.Group
+            onChange={onChange}
+            value={poolType}
+            options={[
+              { value: Pool.PoolType.Base, label: 'Base' },
+              { value: Pool.PoolType.Quote, label: 'Quote' },
+            ]}
+          />
+        </div>
       </div>
       <div className={'flex gap-[10px]'} >
         <label>TP: </label>
