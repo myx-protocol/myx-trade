aaaaff54e5e2ef38f8ba8339e7950935300c1090
CiCi202401 <cici@myx.finance>
2025-10-23 11:34:22 +0800

    feat: pool detail

--- src/lp/pool/index.ts
+++ src/lp/pool/index.ts
@@ -5,4 +5,5 @@ export { getUserGenesisShare } from "@/lp/pool/getUserGenesisShare";
 export {addTpSl} from "./addTpSl";
 export {cancelTpSl} from "./cancel";
 export {reprime} from "./reprime";
+export {getPoolInfo as getPoolDetail} from '../getPoolInfo'
 export * from './type'
