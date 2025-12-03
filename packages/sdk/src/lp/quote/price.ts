68a425a67528096a9f0f44b98d3948c17a796893
CiCi202401 <CiCi202401@163.com>
2025-12-03 19:45:21 +0800

    feat: sdk lp price update

--- packages/sdk/src/lp/quote/price.ts
+++ packages/sdk/src/lp/quote/price.ts
@@ -13,12 +13,12 @@ export const getLpPrice = async (chainId:ChainId,poolId: string) => {
     const pool = await getPoolInfo (chainId,poolId);
     const contract = await getQuotePoolContract(chainId);
     let price = 0n
-    if (!(Number(pool?.state) === MarketPoolState.Cook || Number(pool?.state) === MarketPoolState.Primed)) {
+    // if (!(Number(pool?.state) === MarketPoolState.Cook || Number(pool?.state) === MarketPoolState.Primed)) {
       const res = await getPriceData(chainId, poolId)
       if (res?.price) {
         price = parseUnits(res.price, COMMON_PRICE_DECIMALS)
       }
-    }
+    // }
     
     const data = await contract.getPoolTokenPrice(poolId, price)
     // console.log( `pool ${poolId} price: `, data)
