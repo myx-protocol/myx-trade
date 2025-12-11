98fff18abe868445dbaa802a8e325e977c838970
Allen <allen@myx.finance>
2025-12-11 17:01:27 +0800

    feat: 🎸 update sdk pool symbols all

--- packages/sdk/src/api/pool/index.ts
+++ packages/sdk/src/api/pool/index.ts
@@ -3,6 +3,21 @@ import { http } from "../request";
 import { baseUrl } from "..";
 
 export const getPoolList = async () => {
-  return http.get<ApiResponse<any[]>>(`${baseUrl}/openapi/gateway/scan/market/list`);
+  return http.get<ApiResponse<any[]>>(
+    `${baseUrl}/openapi/gateway/scan/market/list`
+  );
+};
 
+export interface PoolSymbolAllResponse {
+  chainId: number;
+  marketId: string;
+  poolId: string;
+  baseSymbol: string;
+  quoteSymbol: string;
+  baseTokenIcon: string;
 }
+export const getPoolSymbolAll = async () => {
+  return http.get<ApiResponse<PoolSymbolAllResponse[]>>(
+    `${baseUrl}/openapi/gateway/scan/pools`
+  );
+};
