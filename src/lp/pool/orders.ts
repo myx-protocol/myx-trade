778b834bb3e63e7076fa5322a54eac1cccb510e6
ryan <fanjiachangryan@163.com>
2025-12-11 09:50:13 +0800

    feat: update

--- src/lp/pool/orders.ts
+++ src/lp/pool/orders.ts
@@ -2,7 +2,7 @@ import { getPoolOpenOrders } from "@/api";
 import { ChainId } from "@/config/chain";
 import { MxSDK } from "@/web3";
 
-export const getOpenOrders = async (chainId:ChainId) => {
+export const getOpenOrders = async (chainId: ChainId, address: string) => {
   try {
     // 自动获取 accessToken，如果没有或过期会自动刷新
     const accessToken = await MxSDK.getInstance().getConfigManager()?.getAccessToken();
@@ -11,9 +11,9 @@ export const getOpenOrders = async (chainId:ChainId) => {
         "Failed to obtain accessToken"
       )
     }
-    const response = await getPoolOpenOrders(accessToken,chainId )
+    const response = await getPoolOpenOrders(accessToken, address, chainId)
     return response.data || [];
-    
+
   } catch (error) {
     console.error(error);
     throw error;
