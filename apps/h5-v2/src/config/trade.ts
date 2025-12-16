46c241a7622cbf36aa5733d04125e789b9f6559f
ryan <fanjiachangryan@163.com>
2025-12-16 17:39:03 +0800

    feat: update

--- apps/h5-v2/src/config/trade.ts
+++ apps/h5-v2/src/config/trade.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_PAIR_PATH =
-  '/trade/421614/0x0f3a23ce1f81d792bb74b7e748d7ee0c0c459db3c5fd06e1c792935f9a90fce0'
+  '/trade/421614/0x8f1a46ef081575b8dc07c52844c7a9b8e2bfde249eae9ddba82df0a8eb6f6b31'
 
 export const DEFAULT_PRICE_PATH =
   '/trade/421614/0x0f3a23ce1f81d792bb74b7e748d7ee0c0c459db3c5fd06e1c792935f9a90fce0'
