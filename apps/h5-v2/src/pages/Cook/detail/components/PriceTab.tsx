4cbffdec4c99975ef53e9f0af91af698019399b7
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2025-12-24 16:03:42 +0800

    fix: ui

--- apps/h5-v2/src/pages/Cook/detail/components/PriceTab.tsx
+++ apps/h5-v2/src/pages/Cook/detail/components/PriceTab.tsx
@@ -1,7 +1,5 @@
 import { Box } from '@mui/material'
-import { Charts } from '@/components/CookDetail/Charts'
 import { usePoolContext } from '@/pages/Cook/hook'
-import { Change } from '@/components/Change.tsx'
 import { formatNumberPrecision } from '@/utils/formatNumber.ts'
 import { COMMON_PRICE_DISPLAY_DECIMALS } from '@/constant/decimals.ts'
 import { RiseFallTextPrecent } from '@/components/RiseFallText/RiseFallTextPrecent.tsx'
@@ -9,32 +7,69 @@ import { useMarketStore } from '@/components/Trade/store/MarketStore.tsx'
 import { formatNumber } from '@/utils/number.ts'
 import { Mode } from '@/pages/Cook/type.ts'
 import { TradingInfo } from '@/pages/Cook/detail/components/TradingInfo.tsx'
+import { ChartInterval } from '@/pages/Earn/type.ts'
+import { t } from '@lingui/core/macro'
+import { useState } from 'react'
+import { AreaCharts } from '@/components/CookDetail/Charts/AreaCharts.tsx'
+import clsx from 'clsx'
 
 export const PriceTab = () => {
   const { baseLpDetail, price, poolId, mode } = usePoolContext()
   const tickerData = useMarketStore((state) => state.tickerData[poolId || ''])
 
+  const resolutionOptions: {
+    label: string
+    value: ChartInterval
+  }[] = [
+    { label: t`Day`, value: ChartInterval.day },
+    { label: t`Week`, value: ChartInterval.week },
+    { label: t`All`, value: ChartInterval.all },
+  ]
+  const [resolution, setResolution] = useState<ChartInterval>(ChartInterval.day)
+
   return (
     <Box>
-      <Box className={'p-[16px]'}>
-        <Box className={'text-[28px] leading-[1] font-[700]'}>
-          <span className={mode === Mode.Rise ? 'text-rise' : 'text-fall'}>
-            ${formatNumberPrecision(price, COMMON_PRICE_DISPLAY_DECIMALS)}
-          </span>
+      <Box className={'flex justify-between px-[16px] py-[20px]'}>
+        <Box className={'flex-1'}>
+          <Box className={'text-[28px] leading-[1] font-[700]'}>
+            <span className={mode === Mode.Rise ? 'text-rise' : 'text-fall'}>
+              ${formatNumberPrecision(price, COMMON_PRICE_DISPLAY_DECIMALS)}
+            </span>
+          </Box>
+          <Box
+            className={'mt-[2px] flex items-center gap-[10px] text-[12px] leading-[1] font-[500]'}
+          >
+            <span className={'text-regular'}>
+              $
+              {tickerData?.price
+                ? formatNumber(tickerData?.price, {
+                    showUnit: false,
+                  })
+                : '--'}
+            </span>
+            <RiseFallTextPrecent value={Number(baseLpDetail?.lpPriceChange)} />
+          </Box>
         </Box>
-        <Box className={'mt-[2px] flex items-center gap-[10px] text-[12px] leading-[1] font-[500]'}>
-          <span className={'text-regular'}>
-            $
-            {tickerData?.price
-              ? formatNumber(tickerData?.price, {
-                  showUnit: false,
-                })
-              : '--'}
-          </span>
-          <RiseFallTextPrecent value={Number(baseLpDetail?.lpPriceChange)} />
-        </Box>
+
+        <div
+          className={clsx(
+            'flex h-[24px] items-center gap-[0] text-[12px] leading-[1] font-[500] font-medium text-[#9397A3]',
+          )}
+        >
+          {resolutionOptions.map((option) => (
+            <div
+              key={option.value}
+              role="button"
+              className={`px-[12px] py-[6px] transition-all ${option.value === resolution ? 'text-green bg-brand-10' : 'text-regular bg-none'}`}
+              onClick={() => setResolution(option.value)}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
       </Box>
-      <Charts />
+
+      <AreaCharts interval={resolution} />
 
       <TradingInfo />
     </Box>
