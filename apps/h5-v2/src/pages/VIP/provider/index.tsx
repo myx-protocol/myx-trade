2802d7319ffa522460ada80d1686769711425a7b
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2026-01-08 17:30:11 +0800

    feat: vip

--- apps/h5-v2/src/pages/VIP/provider/index.tsx
+++ apps/h5-v2/src/pages/VIP/provider/index.tsx
@@ -2,6 +2,11 @@ import { VIPContext } from '@/pages/VIP/context.ts'
 import type { ReactNode } from 'react'
 import { useFetchLevelList, useFetchUserVipInfo } from '@/hooks/vip/useVipLevel.ts'
 import Container from '@/components/Container.tsx'
+import { VIPCard } from '@/pages/VIP/components/VipCard.tsx'
+import { Upgrade } from '@/pages/VIP/components/Upgrade.tsx'
+import { VIPLevel } from '@/pages/VIP/components/VipList.tsx'
+import { SecondHeader } from '@/components/SecondHeader'
+import { Trans } from '@lingui/react/macro'
 
 export const VIPProvider = ({ children }: { children: ReactNode }) => {
   const { riskTier, setRiskTier, riskList, levelList, feeMap, isFeeLoading } = useFetchLevelList()
@@ -19,6 +24,7 @@ export const VIPProvider = ({ children }: { children: ReactNode }) => {
         isFeeLoading,
       }}
     >
+      <SecondHeader title={<Trans>VIP</Trans>} />
       <div className={'flex flex-col overflow-x-hidden px-[16px] py-[24px]'}>{children}</div>
     </VIPContext.Provider>
   )
