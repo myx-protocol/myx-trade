b0c1b8c5b9f78d4a892df7923953cf88b7e7b971
Allen <allen@myx.finance>
2026-01-15 21:17:44 +0800

    feat: 🎸 update appeal

--- src/manager/api/appeal-type.ts
+++ src/manager/api/appeal-type.ts
@@ -1,3 +1,5 @@
+import { type Address } from 'viem'
+
 export enum AppealType {
   UnderReview = 1,
   InitialVote = 2,
@@ -28,35 +30,35 @@ export enum AppealStage {
 }
 
 export interface GetAppealListParams {
-  chainId?: number;
-  poolId?: string;
-  type?: AppealType;
-  stage?: AppealStage;
-  before?: number;
-  after?: number;
-  limit?: number;
+  chainId?: number
+  poolId?: string
+  type?: AppealType
+  stage?: AppealStage
+  before?: number
+  after?: number
+  limit?: number
 }
 
 export interface AppealListItem {
-  id: number;
-  chainId: number;
-  caseId: number;
-  poolId: string;
-  complainant: string; // appeal user
-  poolToken: string; // appeal margin token
-  disputeBond: string; // appeal margin amount
-  type: AppealType;
-  stage: AppealStage;
-  totalVoteCount: number; // total vote count
-  votedCount: number; // voted count
-  appealCount: number; // 反诉总数
-  appealDeadline: number; // 反诉截止时间
-  compClaimDeadline: number; // 补偿领取时间
-  updateTime: number; // update time
+  id: number
+  chainId: number
+  caseId: number
+  poolId: string
+  complainant: string // appeal user
+  poolToken: string // appeal margin token
+  disputeBond: string // appeal margin amount
+  type: AppealType
+  stage: AppealStage
+  totalVoteCount: number // total vote count
+  votedCount: number // voted count
+  appealCount: number // 反诉总数
+  appealDeadline: number // 反诉截止时间
+  compClaimDeadline: number // 补偿领取时间
+  updateTime: number // update time
 }
 
 export interface GetAppealDetailParams {
-  caseId: number;
+  caseId: number
 }
 
 export enum NodeTypeEnum {
@@ -70,113 +72,113 @@ export enum VoteTypeEnum {
 }
 
 export interface AppealVoteItem {
-  time: number;
-  node: string;
-  type: NodeTypeEnum;
-  option: VoteTypeEnum;
+  time: number
+  node: string
+  type: NodeTypeEnum
+  option: VoteTypeEnum
 }
 
 export interface AppealReconsiderationItem {
-  respondent: string; // 上诉地址
-  type: AppealReconsiderationType;
-  stage: AppealStage;
-  voteCount: number;
-  totalVoteCount: number;
+  respondent: string // 上诉地址
+  type: AppealReconsiderationType
+  stage: AppealStage
+  voteCount: number
+  totalVoteCount: number
 }
 
 export interface AppealDetail {
-  caseId: number;
-  chainId: number;
-  poolId: string;
-  complainant: string;
-  poolToken: string;
-  disputeBond: string;
-  type: AppealType;
-  stage: AppealStage;
-  totalVoteCount: number;
-  votedCount: number;
-  appealCount: number;
-  appealDeadline: number;
-  voteDeadline: number;
-  epochStartTime: number;
-  epochEndTime: number;
-  publicNoticeTime: number;
-  respondents: string[];
-  compClaimDeadline: number;
-  votes: AppealVoteItem[];
-  appeals: AppealReconsiderationItem[];
-  disputeTime: number; // 申诉时间
-  txHash: string; // 申诉交易hash
-  disputeBondState: AppealClaimStatusEnum;
+  caseId: number
+  chainId: number
+  poolId: string
+  complainant: string
+  poolToken: string
+  disputeBond: string
+  type: AppealType
+  stage: AppealStage
+  totalVoteCount: number
+  votedCount: number
+  appealCount: number
+  appealDeadline: number
+  voteDeadline: number
+  epochStartTime: number
+  epochEndTime: number
+  publicNoticeTime: number
+  respondents: string[]
+  compClaimDeadline: number
+  votes: AppealVoteItem[]
+  appeals: AppealReconsiderationItem[]
+  disputeTime: number // 申诉时间
+  txHash: string // 申诉交易hash
+  disputeBondState: AppealClaimStatusEnum
 }
 
 export interface AppealUploadEvidenceParams {
-  caseId: number;
-  poolId: string;
-  evidence: string;
+  caseId: number
+  poolId: string
+  evidence: string
 }
 
 export interface GetAppealReconsiderationListParams {
-  chainId?: number;
-  poolId?: string;
-  type?: AppealReconsiderationType;
-  stage?: AppealStage;
-  before?: number;
-  after?: number;
-  limit?: number;
+  chainId?: number
+  poolId?: string
+  type?: AppealReconsiderationType
+  stage?: AppealStage
+  before?: number
+  after?: number
+  limit?: number
 }
 
 export interface AppealReconsiderationListItem {
-  id: number;
-  chainId: number;
-  caseId: number;
-  poolId: string;
-  respondent: string;
-  type: AppealReconsiderationType;
-  stage: AppealStage;
-  voteCount: number;
-  totalVoteCount: number;
-  appealToken: string; // margin token address
-  appealBond: string; // margin amount
-  appealDeadline: number; // appeal deadline
-  publicNoticeEndTime: number; // public notice end time
-  updateTime: number; // update time
+  id: number
+  chainId: number
+  caseId: number
+  poolId: string
+  respondent: string
+  type: AppealReconsiderationType
+  stage: AppealStage
+  voteCount: number
+  totalVoteCount: number
+  appealToken: string // margin token address
+  appealBond: string // margin amount
+  appealDeadline: number // appeal deadline
+  publicNoticeEndTime: number // public notice end time
+  updateTime: number // update time
 }
 
 export interface GetAppealReconsiderationDetailParams {
-  caseId: number;
-  appealCaseId?: number;
+  caseId: number
+  appealCaseId?: number
 }
 
 export interface AppealReconsiderationDetail {
-  id: number;
-  chainId: number;
-  poolId: string;
-  complainant: string;
-  respondent: string;
-  type: AppealReconsiderationType;
-  stage: AppealStage;
-  disputeTotalVoteCount: number;
-  disputeVotedCount: number;
-  disputeTxHash: string;
-  disputeVoteDeadline: number;
-  disputeStartTime: number;
-  disputeVotes: AppealVoteItem[]; // 初审投票
-  publicNoticeStartTime: number;
-  publicNoticeEndTime: number;
-  appealDeadline: number; // 反诉结束时间
-  respondents: string[]; // 操作者列表
-  appealToken: string; // margin token address
-  appealBond: string; // margin amount
-  appealVotes: AppealVoteItem[]; // 反诉投票
-  appealTotalVoteCount: number; //可投票总数
-  appealVotedCount: number; // 已投票数
+  id: number
+  chainId: number
+  poolId: string
+  complainant: string
+  respondent: string
+  type: AppealReconsiderationType
+  stage: AppealStage
+  disputeTotalVoteCount: number
+  disputeVotedCount: number
+  disputeTxHash: string
+  disputeVoteDeadline: number
+  disputeStartTime: number
+  disputeVotes: AppealVoteItem[] // 初审投票
+  publicNoticeStartTime: number
+  publicNoticeEndTime: number
+  appealDeadline: number // 反诉结束时间
+  respondents: string[] // 操作者列表
+  appealToken: string // margin token address
+  appealBond: string // margin amount
+  appealVotes: AppealVoteItem[] // 反诉投票
+  appealTotalVoteCount: number //可投票总数
+  appealVotedCount: number // 已投票数
 }
 
 export interface AppealReimbursementParams {
-  before?: number;
-  after?: number;
-  limit?: number;
+  before?: number
+  after?: number
+  limit?: number
 }
 
 export enum AppealClaimStatusEnum {
@@ -185,39 +187,86 @@ export enum AppealClaimStatusEnum {
 }
 
 export interface AppealReimbursementItem {
-  id: number;
-  caseId: number;
-  chainId: number;
-  poolId: string;
-  baseAmount: string;
-  quoteAmount: string;
-  claimTime: number;
-  expireTime: number;
-  createTime: number;
-  claimStatus: AppealClaimStatusEnum;
+  id: number
+  caseId: number
+  chainId: number
+  poolId: string
+  baseAmount: string
+  quoteAmount: string
+  claimTime: number
+  expireTime: number
+  createTime: number
+  claimStatus: AppealClaimStatusEnum
 }
 
 export interface GetAppealNodeVoteListParams {
-  before?: number;
-  after?: number;
-  limit?: number;
+  before?: number
+  after?: number
+  limit?: number
+}
+
+export enum AppealNodeVotedStateEnum {
+  NotVoted = 0,
+  Voted = 1,
+}
+
+export enum AppealCaseTypeEnum {
+  Appeal = 1,
+  Reconsideration = 2,
+}
+
+export enum AppealNodeStateEnum {
+  NoVote = 0,
+  Supported = 1,
+  Rejected = 2,
+}
+
+export interface AppealNodeVoteListItem {
+  id: number
+  chainId: number
+  caseId: number
+  poolId: string
+  appealCaseId: number
+  type: AppealCaseTypeEnum
+  deadline: number
+  votedCount: number
+  totalVoteCount: number
+  voteOption: VoteTypeEnum
+  state: AppealNodeVotedStateEnum
+  nodeState: AppealNodeStateEnum
 }
 
 export interface AppealNodeVoteItem {
-  voteDeadline: number;
-  disputeEvidenceUrl: string;
-  appealEvidenceUrl: NodeTypeEnum;
-  disputeRemark: VoteTypeEnum;
-  appealRemark: string;
-  respondents: string[];
-  respondent: string;
+  voteDeadline: number
+  disputeEvidenceUrl: string
+  appealEvidenceUrl: NodeTypeEnum
+  disputeRemark: VoteTypeEnum
+  appealRemark: string
+  respondents: string[]
+  respondent: string
 }
 
 export interface GetIsVoteNodeParams {
-  account: string;
+  account: string
 }
 
 export enum IsVoteNodeEnum {
   Yes = 1,
   No = 0,
 }
+
+export interface GetAppealVoteNodeDetailParams {
+  caseId: number
+  appealCaseId?: number
+}
+
+export interface PostVoteSignatureParams {
+  chainId: number
+  caseId: number
+  type: AppealCaseTypeEnum
+  validator: Address
+  voteOption: VoteTypeEnum
+  signature: string
+}
+
+export type PostVoteResponse = 1 | 0
