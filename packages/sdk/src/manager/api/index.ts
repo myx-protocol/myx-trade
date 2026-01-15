99b36f2e7203d80464d24ec7cfb2c40bd3275ca6
Allen <allen@myx.finance>
2026-01-15 14:48:40 +0800

    feat: 🎸 update appeal

--- packages/sdk/src/manager/api/index.ts
+++ packages/sdk/src/manager/api/index.ts
@@ -58,6 +58,8 @@ import { addQueryParams } from "@/api/utils";
 import { ChainId } from "@/config/chain";
 import { Request } from "./request";
 
+export * from "./appeal-type";
+
 export class Api extends Request {
   private logger: Logger;
   constructor(configManager: ConfigManager, logger: Logger) {
