87c646f8ea61ea5da982fee1f914b3b25d8602b5
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2026-01-27 17:06:27 +0800

    feat: base token state
    (cherry picked from commit 17e4ec77eb64666d9a109e502d02fa993a9e42e1)

---
+++ apps/h5-v2/src/config/error/CommonErrorMapping.tsx
@@ -0,0 +1,12 @@
+import { Trans } from '@lingui/react/macro'
+import type { ReactElement } from 'react'
+
+export const CommonErrorMapping: Record<string, ReactElement> = {
+  'contract runner does not support gas estimation': (
+    <Trans>contract runner does not support gas estimation</Trans>
+  ),
+  'Upfront cost exceeds account balance (transaction up-front cost 0x7000000 exceeds transaction sender account balance 0x0)':
+    <Trans>Upfront cost exceeds account balance</Trans>,
+  UserRejectedRequestError: <Trans>User Rejected</Trans>,
+  ACTION_REJECTED: <Trans>User Rejected</Trans>,
+}
