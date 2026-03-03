7c31b48cf3ec8f8803fb58a3de91c2982f0a820e
CiCi202401 <CiCi202401@163.com>
2026-03-03 12:25:04 +0800

    feat: update error map

---
+++ src/script/parseErrorsToJson.mjs
@@ -0,0 +1,26 @@
+import fs from "fs";
+
+const text = fs.readFileSync("./errors", "utf8");
+
+const regex =
+  /│\s*\d+\s*│\s*'([0-9a-f]{8})'\s*│\s*'([^']+)'\s*│/gi;
+
+const result = {};
+let match;
+
+while ((match = regex.exec(text)) !== null) {
+  const selector = match[1];
+  const name = match[2];
+  result[`0x${selector}`] = name;
+}
+
+const tsContent = `export const customErrorMapping: Record<string, string> = ${JSON.stringify(
+  result,
+  null,
+  2
+)} as const;
+`;
+
+fs.writeFileSync("./../config/customErrorMap.ts", tsContent, "utf8");
+
+console.log(`✅ 成功解析 ${Object.keys(result).length} 条`);
