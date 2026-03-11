b96930fa86842ba03b66f7732140bc5a7d6046c0
Allen <allen@myx.finance>
2026-03-11 16:14:46 +0800

    feat: 🎸 custom debug

--- packages/sdk/src/crypto/md5.ts
+++ packages/sdk/src/crypto/md5.ts
@@ -1,5 +1,5 @@
 import Hex from 'crypto-js/enc-hex'
-import cryptoMD5 from 'crypto-js/md5'
+import cryptoMD5 from 'crypto-js/md5.ts'
 
 export const md5 = (message: string, length?: 16 | 32) => {
   return cryptoMD5(message, { length: length || 32 }).toString(Hex)
