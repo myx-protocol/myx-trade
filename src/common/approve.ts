d7abd0b36af903290344dc39ba3f6e2c2dc1ebc8
CiCi202401 <CiCi202401@163.com>
2026-03-18 18:17:20 +0800

    feat: approve

--- src/common/approve.ts
+++ src/common/approve.ts
@@ -1,13 +1,21 @@
 import { getERC20Contract } from "@/web3/providers.js";
 import { getPublicClient } from "@/web3/viemClients.js";
 import { ChainId } from "@/config/chain.js";
-import { type Address } from "@/api";
+import { type Address } from "@/api/index.js";
+import { bigintTradingGasPriceWithRatio, bigintTradingGasToRatioCalculator } from "@/common/tradingGas.ts";
+import { CHAIN_INFO } from "@/config/chains/index.js";
 
 export const approve = async (chainId: ChainId, _account: string, tokenAddress: string, approveAddress: string, amount: bigint) => {
   try {
+    const chainInfo = CHAIN_INFO[chainId];
     const contract = await getERC20Contract(chainId, tokenAddress);
     if (!contract.write) throw new Error("Wallet client required for write");
-    const hash = await contract.write.approve([approveAddress as Address, amount]) as Address;
+    // 1️⃣ estimate gas
+    const _gasLimit = await contract!.estimateGas!.approve([approveAddress as Address, amount]);
+    const gasLimit = bigintTradingGasToRatioCalculator (_gasLimit, chainInfo.gasLimitRatio)
+    const { gasPrice } = await bigintTradingGasPriceWithRatio (chainId)
+    
+    const hash = await contract.write.approve([approveAddress as Address, amount],{gasLimit, gasPrice}) as Address;
     const client = getPublicClient(chainId);
     await client.waitForTransactionReceipt({ hash });
   } catch (e) {
