077796e5793432c203cdc3bc1657ac147553c9ae
CiCi202401 <CiCi202401@163.com>
2026-03-18 16:44:41 +0800

    feat: address type

--- src/common/transfer.ts
+++ src/common/transfer.ts
@@ -1,12 +1,13 @@
 import { ChainId } from "@/config/chain.js";
 import { getERC20Contract } from "@/web3/providers.js";
 import { getPublicClient } from "@/web3/viemClients.js";
+import { type Address } from "@/api";
 
 export const transfer = async (chainId: ChainId, tokenAddress: string, approveAddress: string, amount: bigint) => {
   try {
     const tokenContract = await getERC20Contract(chainId, tokenAddress);
     if (!tokenContract.write) throw new Error("Wallet client required for write");
-    const hash = await tokenContract.write.transfer([approveAddress as `0x${string}`, amount]) as `0x${string}`;
+    const hash = await tokenContract.write.transfer([approveAddress as Address, amount]) as Address;
     const client = getPublicClient(chainId);
     await client.waitForTransactionReceipt({ hash });
   } catch (e) {
