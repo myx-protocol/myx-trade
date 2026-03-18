5aab2446ce767a7001025f844f57ce4e03b7a1f7
CiCi202401 <CiCi202401@163.com>
2026-03-18 16:44:41 +0800

    feat: address type

--- packages/sdk/src/common/approve.ts
+++ packages/sdk/src/common/approve.ts
@@ -1,12 +1,13 @@
 import { getERC20Contract } from "@/web3/providers.js";
 import { getPublicClient } from "@/web3/viemClients.js";
 import { ChainId } from "@/config/chain.js";
+import { type Address } from "@/api";
 
 export const approve = async (chainId: ChainId, _account: string, tokenAddress: string, approveAddress: string, amount: bigint) => {
   try {
     const contract = await getERC20Contract(chainId, tokenAddress);
     if (!contract.write) throw new Error("Wallet client required for write");
-    const hash = await contract.write.approve([approveAddress as `0x${string}`, amount]) as `0x${string}`;
+    const hash = await contract.write.approve([approveAddress as Address, amount]) as Address;
     const client = getPublicClient(chainId);
     await client.waitForTransactionReceipt({ hash });
   } catch (e) {
