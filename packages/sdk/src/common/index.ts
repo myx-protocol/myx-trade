1df8f3423da9994a2acc35bf28ec772165fdf24e
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2026-03-19 16:09:24 +0800

    feat: restore format utils

--- packages/sdk/src/common/index.ts
+++ packages/sdk/src/common/index.ts
@@ -4,14 +4,5 @@ export * from './balanceOf.js'
 export * from './tradingGas.js'
 export * from './tokenInfo.js'
 
-import { formatUnits as _formatUnits, parseUnits  } from "viem";
+export { formatUnits, parseUnits } from "viem";
 
-
-export const formatUnits = (value: undefined | null | bigint,  decimals: number) => {
-  if (value === undefined || value === null ) {
-    return value
-  }
-  return _formatUnits(value, decimals)
-}
-
-export { parseUnits }
