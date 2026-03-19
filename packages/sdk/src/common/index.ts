81fb6beebdbbfd0402456ec63356983722948da5
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2026-03-19 15:30:03 +0800

    feat: sdk

--- packages/sdk/src/common/index.ts
+++ packages/sdk/src/common/index.ts
@@ -3,3 +3,15 @@ export * from './allowance.js'
 export * from './balanceOf.js'
 export * from './tradingGas.js'
 export * from './tokenInfo.js'
+
+import { formatUnits as _formatUnits, parseUnits  } from "viem";
+
+
+export const formatUnits = (value: undefined | null | bigint,  decimals: number) => {
+  if (value === undefined || value === null ) {
+    return value
+  }
+  return _formatUnits(value, decimals)
+}
+
+export { parseUnits }
