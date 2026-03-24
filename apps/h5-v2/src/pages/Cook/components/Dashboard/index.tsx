86314d310e3e6201e8ac985dc23e97b433189e97
CiCi202401 <138768192+time-endless@users.noreply.github.com>
2026-03-24 12:15:11 +0800

    feat: locale

--- apps/h5-v2/src/pages/Cook/components/Dashboard/index.tsx
+++ apps/h5-v2/src/pages/Cook/components/Dashboard/index.tsx
@@ -6,7 +6,7 @@ import { TradeVolume } from '@/pages/Cook/components/Dashboard/TradeVolume.tsx'
 export const Dashboard = () => {
   return (
     <Box className="px-[16px] pb-[16px]">
-      <Box>
+      <Box className={'text-[16px] leading-[1] font-[700]'}>
         <Trans>Dashboard</Trans>
       </Box>
       <Box className="mt-[12px] flex h-[104px] gap-[8px]">
