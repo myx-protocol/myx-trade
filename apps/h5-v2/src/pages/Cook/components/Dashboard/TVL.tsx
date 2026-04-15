a6c404ec6dd626c95e48ddf28667e2655551ffc4
CiCi202401 <163803564+CiCi202401@users.noreply.github.com>
2026-04-15 10:53:41 +0800

    feat: tvl
    (cherry picked from commit afbd7d366fee05867d0d71753d72a82f1d27c030)
    (cherry picked from commit a14b32936c8b017a84a4c1e1c03c2246b8b35292)
    (cherry picked from commit aea3d75d6a2dd334f43c42034281dea6cb33b3fa)

--- apps/h5-v2/src/pages/Cook/components/Dashboard/TVL.tsx
+++ apps/h5-v2/src/pages/Cook/components/Dashboard/TVL.tsx
@@ -22,9 +22,7 @@ export const TVL = () => {
     refetchInterval: 1000 * 60,
   })
   const totalTvl = useMemo(() => {
-    return (data || []).reduce((previousValue, currentValue) => {
-      return previousValue.add(currentValue.value || 0)
-    }, new Big(0))
+    return (data || []).sort((a, b) => b.time - a.time)?.[0]?.value
   }, [data])
 
   const setData = useCallback((list: LpPriceHistory[]) => {
@@ -56,7 +54,7 @@ export const TVL = () => {
     }
   }, [data, setData])
   return (
-    <Statistic title={<Trans>Tvl</Trans>} value={totalTvl.toString()}>
+    <Statistic title={<Trans>Tvl</Trans>} value={totalTvl || ''}>
       <Box className={'pointer-events-none h-full w-full'} ref={lineRef}></Box>
     </Statistic>
   )
