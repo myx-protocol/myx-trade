35e01d48752b1f25da77b013ff2f0373618935d4
CiCi202401 <163803564+CiCi202401@users.noreply.github.com>
2026-04-17 14:56:07 +0800

    feat: total tvl and trade volume
    (cherry picked from commit 800924a7f44f5a0bbe339d7a96d782a6aae62698)

--- apps/h5-v2/src/pages/Cook/components/Dashboard/TVL.tsx
+++ apps/h5-v2/src/pages/Cook/components/Dashboard/TVL.tsx
@@ -17,12 +17,13 @@ export const TVL = () => {
     queryKey: ['getTrenchTvl'],
     queryFn: async () => {
       const data = await getTrenchTvl()
-      return data?.data || []
+      return (data?.data || []).sort((a, b) => a.time - b.time)
     },
     refetchInterval: 1000 * 60,
   })
   const totalTvl = useMemo(() => {
-    return (data || []).sort((a, b) => b.time - a.time)?.[0]?.value
+    if (!data?.length) return undefined
+    return (data || []).reduce((max, item) => (item.time > max.time ? item : max)).value
   }, [data])
 
   const setData = useCallback((list: LpPriceHistory[]) => {
