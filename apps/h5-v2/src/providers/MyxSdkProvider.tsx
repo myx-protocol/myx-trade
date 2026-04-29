1cc726b4cb6c8ba5b8c4ca5496a6f7f5fd646c57
Allen <allen@myx.finance>
2026-04-29 18:35:25 +0800

    feat: 🎸 update testnet contract address

--- apps/h5-v2/src/providers/MyxSdkProvider.tsx
+++ apps/h5-v2/src/providers/MyxSdkProvider.tsx
@@ -145,8 +145,8 @@ export const useMyxSdkClient = (chainId?: number) => {
 const brokerAddressMap: Record<number, string> = {
   [ChainId.ARB_TESTNET]: isBetaMode()
     ? '0x4A3054177DBdC01BfcA007FB45d9A9803eBc2eA4'
-    : '0x69a7dC1638B98dD4734e690bE5bAba835d562d9e',
-  [ChainId.LINEA_SEPOLIA]: isBetaMode() ? '' : '0x6C4655D0034c74f82B3769749cacDb6Df5cC4862',
+    : '0x31e65de738482Dc2740eBE601a0e28A9d81De2a8',
+  [ChainId.LINEA_SEPOLIA]: isBetaMode() ? '' : '0xCf60b036DeDCE08441DfAF211E12fC45AdBDd929',
   [ChainId.BSC_TESTNET]: isBetaMode() ? '0x144E5067E690635b2cbeE10D96f431D143739f48' : '',
   [ChainId.BSC_MAINNET]: '0x93152c7Bd0269ecbb53DF247e4f8ebFe3F4a2026',
 }
