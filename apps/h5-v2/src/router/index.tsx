46e865a378e6cc444bfd16cf955b5f1b5049a0b2
CiCi202401 <163803564+CiCi202401@users.noreply.github.com>
2026-05-18 11:38:21 +0800

    feat: add trench detail

--- apps/h5-v2/src/router/index.tsx
+++ apps/h5-v2/src/router/index.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import Market from '@/pages/Market'
 import Cook from '@/pages/Cook'
 import { CookDetail } from '@/pages/Cook/detail/index'
+import { TrenchDetail } from '@/pages/Trench/detail/index'
 import EarnList from '@/pages/Earn/List.tsx'
 import EarnDetail from '@/pages/Earn/detail/index'
 import { Home } from '@/pages/Home'
@@ -73,6 +74,10 @@ const router = createBrowserRouter([
       {
         path: 'cook/:chainId/:poolId',
         element: <CookDetail />,
+      },
+      {
+        path: 'trench/:chainId/:poolId',
+        element: <TrenchDetail />,
       },
       {
         path: 'earn',
