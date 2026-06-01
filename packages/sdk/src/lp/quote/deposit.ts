1806969bb70d673c2f651f23c7ec57a252477bf0
CiCi202401 <163803564+CiCi202401@users.noreply.github.com>
2026-06-01 12:55:46 +0800

    feat: common deposit data

--- packages/sdk/src/lp/quote/deposit.ts
+++ packages/sdk/src/lp/quote/deposit.ts
@@ -1,21 +1,14 @@
 import { getAccount } from "@/web3/providers.js";
 import { parseUnits } from "viem";
-import { bigintAmountSlipperCalculator } from "@/common/tradingGas.js";
 import { Deposit } from "@/lp/type.js";
 import { checkParams } from "@/common/checkParams.js";
 import { previewLpAmountOut } from "@/lp/quote/preview.js";
 import { getPoolInfo } from "@/lp/getPoolInfo.js";
-import { getPriceData } from "@/common/price.js";
-import {
-  COMMON_LP_AMOUNT_DECIMALS,
-  COMMON_PRICE_DECIMALS,
-} from "@/config/decimals.js";
-import type { TpSl } from "@/lp/pool/index.js";
-import { getTpSlParams } from "@/common/getTpSlParams.js";
+import { getDepositData } from "@/common/depositData.js";
 import { ErrorCode, Errors, getErrorTextFormError } from "@/config/error.js";
 import { getContractAddressByChainId } from "@/config/address/index.js";
 import { sdkError } from "@/logger";
-import { isNeedPrice } from "@/utils/isNeedPrice";
+import { MarketPoolState } from "@/api/index.js";
 import LiquidityRouter_ABI from "@/abi/LiquidityRouter.json";
 import { signAndSubmit } from "@/common/signAndSubmit";
 
@@ -45,55 +38,26 @@ export const deposit = async (params: Deposit) => {
     });
 
     const amountIn = parseUnits(amount.toString(), decimals);
-
-    const _isNeedPrice = isNeedPrice(pool?.state);
-
-    let amountOut;
-
-    if (_isNeedPrice) {
-      // todo  getprice
-      const priceData = await getPriceData(chainId, poolId);
-      if (!priceData) return;
-      const referencePrice = parseUnits(priceData.price, COMMON_PRICE_DECIMALS);
-      amountOut = await previewLpAmountOut({
-        chainId,
-        poolId,
-        amountIn,
-        price: referencePrice,
-      });
-    } else {
-      amountOut = await previewLpAmountOut({ chainId, poolId, amountIn });
-    }
-
-    const _tpsl = tpsl.map((item) => {
-      return {
-        amount,
-        triggerPrice: item.triggerPrice,
-        triggerType: item.triggerType,
-      } as TpSl;
-    });
-
-    const tpslParams = getTpSlParams(
-      slippage,
-      _tpsl,
-      COMMON_LP_AMOUNT_DECIMALS,
-      decimals,
-    );
-    const minAmountOut = bigintAmountSlipperCalculator(amountOut, slippage);
-    const data = {
+    const result = await getDepositData({
       poolId,
+      chainId,
+      account,
       amountIn,
-      minAmountOut,
-      recipient: account,
-      tpslParams,
-    } as const;
+      amount,
+      slippage,
+      state: pool.state as MarketPoolState,
+      quoteDecimals: decimals,
+      tpsl,
+      previewLpAmountOut,
+    });
+    if (!result) return;
 
     return await signAndSubmit({
       chainId,
       account,
       abi: LiquidityRouter_ABI,
       method: "depositQuote",
-      args: [data],
+      args: [result.data],
       poolIds: [poolId],
     });
   } catch (error) {
